b49898e8ca12d5905a5f4f1fc7604d07442a0ef3
wangxinyue <wangxinyue@weiboyi.com>
2019-03-25 09:47:05 +0800

    调整

--- src/cooperationPlatform/constants/Interface.js
+++ src/cooperationPlatform/constants/Interface.js
@@ -1,6 +1,6 @@
 const defaultUrl = 'operator-gateway/trinityPlatform/v1/'
-const skuUrl = '/operator-gateway/trinitySku/v1/'
-const agent = '/operator-gateway/trinityAgent/v1/'
+const skuUrl = 'operator-gateway/trinitySku/v1/'
+const agent = 'operator-gateway/trinityAgent/v1/'
 export default {
 	'getCooperationPlatformByPage': defaultUrl + 'getCooperationPlatformByPage',//合作平台-管理列表接口
 	'insertCooperationPlatform': defaultUrl + 'insertCooperationPlatform',//合作平台-新增接口
@@ -12,7 +12,7 @@ export default {
 	'getExistTrinityPlatformList': defaultUrl + 'getExistTrinityPlatformList',//合作平台-所属媒体平台下拉框接口
 	'getCooperationPlatform': defaultUrl + 'getCooperationPlatform',//获取除当前启用平台外是否还有其他启用平台
 
-	'getPaymentTypeList': defaultUrl + 'getPaymentTypeList',//合作平台-开户行下拉框接口
+	'getPaymentTypeList': 'finance/payment/getPaymentTypeList',//合作平台-开户行下拉框接口
 	'': defaultUrl + '',//合作平台-获取合作平台是否有启用的报价项
 	'getUnusedCooperationPlatform': defaultUrl + 'getUnusedCooperationPlatform',//获取未使用关联的抓取合作平台
 	'getAgentByPage': agent + 'getAgentByPage',//代理商列表
