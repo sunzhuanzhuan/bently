04bbe08f6e5e27f7715bc91f684fbe7fce19c975
付羽 <fuyu@weiboyi.com>
2019-04-23 18:14:44 +0800

    关闭showlog

--- src/util/sensor/sensors.js
+++ src/util/sensor/sensors.js
@@ -23,7 +23,7 @@ export const sensors = (userId, serviceUrl, appId) => {
 			},
 			scroll_delay_time: 4000
 		},
-		show_log: true
+		show_log: false
 	})
 	wby.login(userId)
 	wby.quick("autoTrack");
