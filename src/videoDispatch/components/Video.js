ec8fbc9f800676ad03c68b857d93db142e87af38
lzb <lzbabd@gmail.com>
2019-04-28 11:53:49 +0800

    新增视频脚本修改

--- src/videoDispatch/components/Video.js
+++ src/videoDispatch/components/Video.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { Form, } from 'antd';
+import { Form, Radio, Input } from 'antd';
 import qs from "qs";
 //视频上传组件
 import VideoUpload from "../base/VideoUpload";
 const FormItem = Form.Item;
+const RadioGroup = Radio.Group;
+const TextArea = Input.TextArea;
 class Video extends Component {
 	constructor(props) {
 		super(props);
@@ -30,8 +32,45 @@ class Video extends Component {
 	}
 	render() {
 		const { videoUpload } = this.state
-		const { formItemLayout, getFieldDecorator, videoTypeContentNow, videoCampaignInfo, VideoUploadParam, isNeedDefault } = this.props
-		return (
+		const { formItemLayout, getFieldDecorator, getFieldValue, videoTypeContentNow, videoCampaignInfo = {}, VideoUploadParam, isNeedDefault } = this.props
+		return videoCampaignInfo.business_account_id ? (
+			<div>
+				<FormItem {...formItemLayout} label="活动形式">
+					{getFieldDecorator('video_type', {
+						initialValue: videoCampaignInfo && parseInt(videoCampaignInfo.video_type) || 2
+					})(
+						<RadioGroup>
+							<Radio value={2}>转发视频</Radio>
+							<Radio value={4}>按脚本拍摄</Radio>
+						</RadioGroup>
+					)}
+				</FormItem>
+				{getFieldValue("video_type") === 4 ? <FormItem {...formItemLayout} label="脚本内容">
+					{getFieldDecorator('video_script', {
+						initialValue: videoCampaignInfo.video_script,
+						rules: [
+							{required: true, message: '请输入脚本内容'},
+							{max: 2000, message: '脚本内容最多不能超过2000个字符'}
+						],
+					})(
+						<TextArea placeholder='请输入脚本内容'/>
+					)}
+					<div className="prompt-content">脚本内容最多不能超过2000个字符</div>
+				</FormItem> : <FormItem {...formItemLayout} label="发布视频">
+					{getFieldDecorator('video', {
+						initialValue: videoUpload,
+						rules: [{
+							required: true, message: '请上传视频文件',
+						}],
+					})(
+						(!isNeedDefault || videoUpload.length > 0) ? <VideoUpload  {...VideoUploadParam} ref={node => this.videoUploadNode = node} /> : <span></span>
+					)}
+					<div className="prompt-content"><span style={{ color: "red" }}>温馨提示：</span>请务必在{videoTypeContentNow.type}平台上试发布该视频，确认其符合{videoTypeContentNow.type}短视频格式
+                        （大小、分辨率、宽高比等），因视频格式不满足{videoTypeContentNow.type}平台要求导致的发布失败，与微播易无关！
+                    </div>
+				</FormItem>}
+			</div>
+		) : (
 			<div>
 				<FormItem {...formItemLayout} label="内容形式">
 					{getFieldDecorator('video_type', {
@@ -50,8 +89,8 @@ class Video extends Component {
 						(!isNeedDefault || videoUpload.length > 0) ? <VideoUpload  {...VideoUploadParam} ref={node => this.videoUploadNode = node} /> : <span></span>
 					)}
 					<div className="prompt-content"><span style={{ color: "red" }}>温馨提示：</span>请务必在{videoTypeContentNow.type}平台上试发布该视频，确认其符合{videoTypeContentNow.type}短视频格式
-                        （大小、分辨率、宽高比等），因视频格式不满足{videoTypeContentNow.type}平台要求导致的发布失败，与微播易无关！
-                    </div>
+						（大小、分辨率、宽高比等），因视频格式不满足{videoTypeContentNow.type}平台要求导致的发布失败，与微播易无关！
+					</div>
 				</FormItem>
 			</div>
 		);
--- src/videoDispatch/configuration/processCampaign.js
+++ src/videoDispatch/configuration/processCampaign.js
@@ -1,14 +1,15 @@
 import moment from 'moment';
 export const processCampaign = (values, uploadToken) => {
+	let video = values.video_type === 4 ? {} : values.video[0]
 	const allValue = {
 		...values,
-		video_path: values.video[0].filepath,
+		video_path: video.filepath,
 		//数据处理
 		note: values.note || "",
 		title_content: values.title_content || "",
 		hot_category_id: values.hot_category_id ? values.hot_category_id[1] : "",
 		link: values.link || "",
-		first_frame_path: values.video[0].cover,
+		first_frame_path: video.cover,
 		is_allow_service: values.is_allow_service ? 1 : 2,
 		start_time: moment(values.start_time).format("YYYY-MM-DD HH:mm:ss"),
 		last_feedback_at: values.is_last_feedback == 1 ? moment(values.last_feedback_at).format("YYYY-MM-DD HH:mm:ss") : "",
