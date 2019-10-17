758c588117059ba2b8deefa8a7b8738270754989
wangxinyue <wangxinyue@weiboyi.com>
2019-10-17 12:50:44 +0800

    区分 0  和  null

--- src/queryExportTool/base/InfoDIsplay/index.js
+++ src/queryExportTool/base/InfoDIsplay/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 import './index.less'
-
-const InfoDIsplay = ({ title, value, suffix, afterLable, textColor, isString }) => {
+import numeral from "numeral";
+const InfoDIsplay = ({ title, value, suffix, afterLable, textColor, isString, format }) => {
 
 	return <div className='item-footer-info-diaplay'>
 		<span className='title'>{title}</span> :
-		{isString ? <strong className='info-value'>{value ? <span>{value}{afterLable}</span> : " - "}</strong> :
-			<strong className='info-value'>{value > 0 ? <span>{value}{afterLable}</span> : " - "}</strong>
+		{isString ? <strong className='info-value'>{value > 0 || value == 1 ? <span>{numeral(value).format(format)}{afterLable}</span> : " - "}</strong> :
+			<strong className='info-value'>{value > 0 || value == 0 ? <span>{value}{afterLable}</span> : " - "}</strong>
 		}
 	</div>
 }
