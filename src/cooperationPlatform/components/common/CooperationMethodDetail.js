33008e537001b3b19b4e0666a4d071e1c4b3302a
baikeqi <baikeqii@163.com>
2019-12-16 11:11:23 +0800

    平台管理查看页面增加发票类型及税率展示

--- src/cooperationPlatform/components/common/CooperationMethodDetail.js
+++ src/cooperationPlatform/components/common/CooperationMethodDetail.js
@@ -5,30 +5,20 @@ class CooperationMethodDetail extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {};
-		this.invoiceArr = {
-			1: '增值税专用发票',
-			2: '增值税普通发票'
-		}
 	}
 	render() {
-		const { detailData: { cooperationType, refundRate, cooperationRemark, invoiceType, agentTaxRate } } = this.props
+		const { detailData: { cooperationType, refundRate, cooperationRemark } } = this.props
 		const weekArr = [
 			{ title: "合作方式", content: "周期付款" },
-			{ title: "回票类型", content: this.invoiceArr[invoiceType] },
-			{ title: "发票税率", content: `${Number(agentTaxRate || 0) * 100} %`, isHide: invoiceType != 1  },
-
 			{ title: "返款比例", content: `${numeral(refundRate || 0).format('0.00')} %` }
 		]
 		const otherArr = [
 			{ title: "合作方式", content: "其他" },
-			{ title: "回票类型", content: this.invoiceArr[invoiceType] },
-			{ title: "发票税率", content: `${Number(agentTaxRate || 0) * 100} %`, isHide: invoiceType != 1 },
 			{ title: "说明", content: cooperationRemark },
 		]
-		const dealArr = cooperationType == 1 ? weekArr : otherArr;
 
 		return (
-			<ShowDetailArr arr={dealArr.filter(item => !(item.isHide))} />
+			<ShowDetailArr arr={cooperationType == 1 ? weekArr : otherArr} />
 		);
 	}
 }
