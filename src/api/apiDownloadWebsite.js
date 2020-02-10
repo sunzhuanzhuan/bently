a8a049080425e139d3fa6a0c1ede6092520665dd
wangxinyue <wangxinyue@weiboyi>
2020-02-10 09:52:00 +0800

    调整判断条件

--- src/api/apiDownloadWebsite.js
+++ src/api/apiDownloadWebsite.js
@@ -6,11 +6,11 @@ import { message } from 'antd'
 
 const apiDownload = (opts, filename = 'file') => {
 	opts.baseURL = '/api'
-	//opts.responseType = 'blob'
+	opts.responseType = 'blob'
 	opts.headers = { "X-Access-Token": Cookie.get('token') || '' }
 	axios(opts).then(res => {
-		if (res.data.code === '5000') {
-			message.error(res.data.msg);
+		if (res.data.type === "application/json") {
+			message.error("数量过多，请选择筛选条件过滤部分数据");
 		} else {
 			let blob = new Blob([res.data]);
 			saveAs(blob, filename)
