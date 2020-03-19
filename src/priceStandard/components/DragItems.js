8d52d5a54ccf8d5e2e8bddc2df2bff1fd3f81eea
wangxinyue <wangxinyue@weiboyi>
2020-03-19 17:13:47 +0800

    模拟拖拽

---
+++ src/priceStandard/components/DragItems.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react"
+import { Icon, Input, Divider, Form } from 'antd'
+import { DragSource, DropTarget } from "react-dnd";
+import { findDOMNode } from "react-dom";
+import CardType from './CardType'
+const DRAG_TYPE = 'ITEM'
+
+// 拖拽源事件对象
+const cardSource = {
+	beginDrag(props) {
+		return {
+			id: props.id,
+			index: props.index
+		}
+	},
+	canDrag(props, monitor) {
+		return true
+	}
+}
+// 拖拽目标事件对象
+const cardTarget = {
+	hover(props, monitor, component) {
+		if (!component) {
+			return null
+		}
+		const dragIndex = monitor.getItem().index
+		const hoverIndex = props.index
+		if (dragIndex === hoverIndex) {
+			return
+		}
+		const hoverBoundingRect = (findDOMNode(component)).getBoundingClientRect()
+		const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+		const clientOffset = monitor.getClientOffset()
+		const hoverClientY = (clientOffset).y - hoverBoundingRect.top
+		if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+			return
+		}
+		if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+			return
+		}
+		props.drag(dragIndex, hoverIndex)
+		monitor.getItem().index = hoverIndex
+	}
+}
+
+@DropTarget(DRAG_TYPE, cardTarget, (connect) => ({
+	connectDropTarget: connect.dropTarget()
+}))
+@DragSource(
+	DRAG_TYPE,
+	cardSource,
+	(connect, monitor) => ({
+		connectDragSource: connect.dragSource(),
+		isDragging: monitor.isDragging()
+	})
+)
+export default class DragItems extends Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			// edit: false,
+			errorProps: {},
+		}
+	}
+
+	render() {
+		const {
+			isDragging,
+			connectDragSource,
+			connectDropTarget,
+			data = {}
+		} = this.props
+		const opacity = isDragging ? 0 : 1
+		console.log("DragItems -> render -> data", data)
+
+		return connectDragSource && connectDropTarget &&
+			connectDragSource(connectDropTarget(
+				<div>
+					<CardType data={data} />
+				</div>
+			))
+	}
+}
--- src/priceStandard/components/SystemList.js
+++ src/priceStandard/components/SystemList.js
@@ -1,11 +1,63 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
+import { Skeleton } from 'antd'
+import debounce from 'lodash/debounce';
+import { DragDropContext } from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
+import DragItems from "./DragItems";
 
-export default class SystemList extends Component {
+const listDefault = [
+	{
+		name: '权益类型：',
+		value: 'ddd',
+		type: 'text'
+	}, {
+		name: '权益单/选设置',
+		value: '单选',
+		type: 'text'
+	}, {
+		name: '权益单/选设置',
+		value: [],
+		type: 'tags'
+	}
+]
+@DragDropContext(HTML5Backend)
+export default class EditSelectedFieldsContent extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			data: [
+				{ id: 1, list: listDefault },
+				{ id: 12, list: listDefault },
+				{ id: 3, list: listDefault },
+				{ id: 4, list: listDefault }
+			]
+		}
+	}
+	handleDrag = (dragIndex, hoverIndex) => {
+		const { data } = this.state
+		console.log("EditSelectedFieldsContent -> handleDrag -> dragIndex, hoverIndex", dragIndex, hoverIndex)
+		data.splice(hoverIndex, 0, ...data.splice(dragIndex, 1))
+		this.setState(data)
+	}
+
+	handleDelete = (deleteIndex) => {
+		console.log("EditSelectedFieldsContent -> handleDelete -> deleteIndex", deleteIndex)
+	}
 	render() {
-		return (
-			<div>
-
+		// 搜索筛选
+		return <div className='fields-selected-area-container'>
+			<div className='content-fields-list'>
+				<div>
+					{this.state.data.map((item, n) =>
+						<DragItems
+							key={item.id}
+							index={n}
+							data={item}
+							drag={this.handleDrag}
+							del={this.handleDelete}
+						/>)}
+				</div>
 			</div>
-		)
+		</div>
 	}
 }
