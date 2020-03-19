b1b132cdf08b3c6331832e8dfb3b1e66df24717e
wangxinyue <wangxinyue@weiboyi>
2020-03-19 17:16:48 +0800

    弹窗事件

--- src/priceStandard/containers/PlatformConfig.js
+++ src/priceStandard/containers/PlatformConfig.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { bindActionCreators } from "redux";
 import * as actions from "../actions";
 import { connect } from "react-redux";
+import { Modal } from 'antd';
 function PlatformConfig() {
+	const [modalProps, setModalProps] = useState({ visible: false, title: '', content: '' })
+	const commonProps = {
+		setModalProps, modalProps
+	}
 	return (
 		<div>
 			<h2>平台权益池配置</h2>
+			<Modal
+				footer={null}
+				{...modalProps}
+				onCancel={() => setModalProps({ visible: false })}
+			>
+				{modalProps.content && modalProps.content(commonProps)}
+			</Modal>
 		</div>
 	)
 }
--- src/priceStandard/containers/SystemConfig.js
+++ src/priceStandard/containers/SystemConfig.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { bindActionCreators } from "redux";
 import * as actions from "../actions";
 import { connect } from "react-redux";
 import TagList from '../components/TagList'
 import SystemList from '../components/SystemList'
-import { Button, Icon } from 'antd';
+import { Button, Icon, Modal } from 'antd';
 
 function SystemConfig() {
+	const [modalProps, setModalProps] = useState({ visible: false, title: '', content: '' })
+	const commonProps = {
+		setModalProps, modalProps
+	}
 	return (
 		<div>
 			<h2>系统权益池管理</h2>
@@ -15,6 +19,13 @@ function SystemConfig() {
 				<span style={{ marginLeft: 31 }}><Icon type="info-circle" theme="filled" className='warning-info' /> 注：若权益已被平台SKU配置，则不可删除。</span>
 			</div>
 			<SystemList />
+			<Modal
+				footer={null}
+				{...modalProps}
+				onCancel={() => setModalProps({ visible: false })}
+			>
+				{modalProps.content && modalProps.content(commonProps)}
+			</Modal>
 		</div>
 	)
 }
