0510339e507a2217d7dd8e8bae929659ce97e3c5
wangxinyue <wangxinyue@weiboyi>
2020-03-19 16:33:08 +0800

    标题样式

---
+++ src/priceStandard/components/SystemList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SystemList extends Component {
+	render() {
+		return (
+			<div>
+
+			</div>
+		)
+	}
+}
--- src/priceStandard/containers/SystemConfig.js
+++ src/priceStandard/containers/SystemConfig.js
@@ -3,14 +3,18 @@ import { bindActionCreators } from "redux";
 import * as actions from "../actions";
 import { connect } from "react-redux";
 import TagList from '../components/TagList'
-import CardType from '../components/CardType'
+import SystemList from '../components/SystemList'
+import { Button, Icon } from 'antd';
 
 function SystemConfig() {
 	return (
 		<div>
 			<h2>系统权益池管理</h2>
-			<TagList />
-			<CardType />
+			<div>
+				<Button type='primary'>+ 添加权益类型</Button>
+				<span style={{ marginLeft: 31 }}><Icon type="info-circle" theme="filled" className='warning-info' /> 注：若权益已被平台SKU配置，则不可删除。</span>
+			</div>
+			<SystemList />
 		</div>
 	)
 }
