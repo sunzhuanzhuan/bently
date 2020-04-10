6c41cdde7ed04ca99c4994d6655b9fd9df2a743f
wangxinyue <wangxinyue@weiboyi>
2020-04-10 15:32:08 +0800

    调整权益参数

--- src/queryExportTool/base/SimpleTables/Equity.js
+++ src/queryExportTool/base/SimpleTables/Equity.js
@@ -5,7 +5,7 @@ import { Tag } from 'antd'
 export default function Equity({ equitiesIdList }) {
 	const [list, setList] = useState([])
 	async function hoverEquity() {
-		const { data } = await api.get('/operator-gateway/equities/v1/getEquitiesListByEquitiesIds', { params: { equitiesIdList: equitiesIdList } })
+		const { data } = await api.get('/operator-gateway/equities/v1/getEquitiesListByEquitiesIds', { params: { equitiesIdList: equitiesIdList.join(',') } })
 		setList(data)
 	}
 	const text = <img src={require('./equity.png')} height='18px' style={{ marginTop: 4, marginLeft: 2 }} />
