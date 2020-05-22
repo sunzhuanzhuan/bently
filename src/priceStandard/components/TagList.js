41855aa70ecb798c904eb81fe9b650158f4ce880
wangxinyue <wangxinyue@weiboyi>
2020-05-22 10:09:12 +0800

    调整按钮显示条件

--- src/priceStandard/components/TagList.js
+++ src/priceStandard/components/TagList.js
@@ -24,6 +24,8 @@ function TagList(props) {
 		setList(deleteList)
 		onChange && onChange(deleteList)
 	}
+
+
 	return (
 		<div className='tag-list'>
 			{
@@ -51,7 +53,8 @@ function TagList(props) {
 								: null}
 						</div>}</div>)
 			}
-			{isOperate && list.length < 20 ? <AddOperate addList={addList} list={list} /> : null}
+			{/*filter item.isDeleted标识未删除的数组集合 */}
+			{isOperate && (list.filter(item => item.isDeleted == 2)).length < 20 ? <AddOperate addList={addList} list={list} key={JSON.stringify(list)} /> : null}
 		</div>
 	)
 }
