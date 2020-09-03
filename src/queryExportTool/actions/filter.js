f58bdf779f849c9de9eaa1e384420870a4fe4199
zhangjia <434497825@qq.com>
2020-09-03 10:45:43 +0800

    格式化代码,没有做功能修改

--- src/queryExportTool/actions/filter.js
+++ src/queryExportTool/actions/filter.js
@@ -7,62 +7,62 @@ const getGroupById = (groups, groupType) => {
     if (groupType === '7' || groupType === '8') {
         groupType = '3';
     }
-	return groups.find(group => group.groupId == groupType)
+    return groups.find(group => group.groupId == groupType)
 }
 const getCategory = (groupedCategories, groupType) => {
-	return groupedCategories.map(item => ({
-		id: item.itemKey,
-		name: item.itemValue
-	}))
+    return groupedCategories.map(item => ({
+        id: item.itemKey,
+        name: item.itemValue
+    }))
 }
 const getOrderIndustryCategory = (orderIndustryCategory = []) => {
-	return orderIndustryCategory.map(item => ({
-		...item,
-		id: item.code,
-	}))
+    return orderIndustryCategory.map(item => ({
+        ...item,
+        id: item.code,
+    }))
 }
 // let version = 0;
 export const getFilters = (params) => (dispatch) => {
-	let { groupType } = params;
+    let {groupType} = params;
     // version = params.version;
-	return api.get(Interface.getFilters).then((data) => {
-		data = data.data;
-		const { groups, groupedCategories, orderIndustryCategory, groupedSkuTypes, kolProvinceList, kolInterestList, defaultHotCities } = data;
+    return api.get(Interface.getFilters).then((data) => {
+        data = data.data;
+        const {groups, groupedCategories, orderIndustryCategory, groupedSkuTypes, kolProvinceList, kolInterestList, defaultHotCities} = data;
         const category = {
-			name: '常见分类',
-			options: getCategory(groupedCategories, groupType)
-		}
-		const operationTags = data.operationTags
-		const group = getGroupById(groups, groupType)
-		// if (version !== params.version) {
-		// 	return;
-		// }
-		dispatch({
-			type: getFilters_success,
-			payload: {
-				[groupType]: {
-					category,
-					operationTags,
-					group,
-					groupedSkuTypes: groupedSkuTypes,
-					orderIndustryCategory: getOrderIndustryCategory(orderIndustryCategory),
-					industryList: data.industryList,
-					kolProvinceList: kolProvinceList,
-					kolInterestList: kolInterestList,
-					defaultHotCities: defaultHotCities.map(item => ({
-						"key": String(item.areaId),
-						"title": item.areaName
-					})),
-					unitPlayPriceTypes: data.unitPlayPriceTypes,
-					unitReadPriceTypes: data.unitReadPriceTypes,
-					verifiedStatus: [
-						{ value: "", name: "不限" },
-						...data.verifiedStatus.map(item => ({ value: item.id, name: item.name }))
-					]
-				}
-			}
-		})
-	})
+            name: '常见分类',
+            options: getCategory(groupedCategories, groupType)
+        }
+        const operationTags = data.operationTags
+        const group = getGroupById(groups, groupType)
+        // if (version !== params.version) {
+        // 	return;
+        // }
+        dispatch({
+            type: getFilters_success,
+            payload: {
+                [groupType]: {
+                    category,
+                    operationTags,
+                    group,
+                    groupedSkuTypes: groupedSkuTypes,
+                    orderIndustryCategory: getOrderIndustryCategory(orderIndustryCategory),
+                    industryList: data.industryList,
+                    kolProvinceList: kolProvinceList,
+                    kolInterestList: kolInterestList,
+                    defaultHotCities: defaultHotCities.map(item => ({
+                        "key": String(item.areaId),
+                        "title": item.areaName
+                    })),
+                    unitPlayPriceTypes: data.unitPlayPriceTypes,
+                    unitReadPriceTypes: data.unitReadPriceTypes,
+                    verifiedStatus: [
+                        {value: "", name: "不限"},
+                        ...data.verifiedStatus.map(item => ({value: item.id, name: item.name}))
+                    ]
+                }
+            }
+        })
+    })
 }
 
 export const {
@@ -72,8 +72,8 @@ export const {
 
 
 export const {
-	getFiltersMeta,
-	getFiltersMeta_success
+    getFiltersMeta,
+    getFiltersMeta_success
 } = createHttpAction('getFiltersMeta', Interface.getFilters)
 
 export const getFilters_success = 'getFilters_success';
