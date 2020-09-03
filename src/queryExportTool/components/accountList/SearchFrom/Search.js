94d1c6dbedc4751841630ed83527ba07107dfd80
zhangjia <434497825@qq.com>
2020-09-03 10:32:21 +0800

    格式化代码,没有做功能修改

--- src/queryExportTool/components/accountList/SearchFrom/Search.js
+++ src/queryExportTool/components/accountList/SearchFrom/Search.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Input, Icon, Checkbox, Row, Col } from 'antd';
+import {Input, Icon, Checkbox, Row, Col} from 'antd';
 import './index.less';
+
 const Search = Input.Search;
 
 export class DefaultSearch extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			userName: '',
-			show: false
-		}
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: '',
+            show: false
+        }
+    }
 
     componentDidMount() {
         document.addEventListener('click', this.handleMouseUp, false)
@@ -26,31 +27,31 @@ export class DefaultSearch extends React.Component {
         });
     }
 
-	emitEmpty = () => {
-		const { onSearch } = this.props;
-		this.userNameInput.focus();
-		// this.setState({ userName: '' });
-		this.userName = '';
-		this.props.form.setFieldsValue({ keyword: '' });
-		onSearch && onSearch({ optionsNames: '' });
-	}
+    emitEmpty = () => {
+        const {onSearch} = this.props;
+        this.userNameInput.focus();
+        // this.setState({ userName: '' });
+        this.userName = '';
+        this.props.form.setFieldsValue({keyword: ''});
+        onSearch && onSearch({optionsNames: ''});
+    }
 
-	componentWillReceiveProps(nextProps) {
-		// debugger;
-		if ("keyword" in nextProps) {
-			this.setState({
-				userName: nextProps.keyword
-			})
-		}
-	}
+    componentWillReceiveProps(nextProps) {
+        // debugger;
+        if ("keyword" in nextProps) {
+            this.setState({
+                userName: nextProps.keyword
+            })
+        }
+    }
 
-	onSearch = () => {
-		const { form } = this.props;
-		const { onSearch } = this.props;
-		const userName = form.getFieldValue('keyword')
-		onSearch && onSearch({ optionsNames: userName || '' });
+    onSearch = () => {
+        const {form} = this.props;
+        const {onSearch} = this.props;
+        const userName = form.getFieldValue('keyword')
+        onSearch && onSearch({optionsNames: userName || ''});
 
-	}
+    }
 
     typeClick = (e) => {
         e.nativeEvent.stopImmediatePropagation();
@@ -63,31 +64,31 @@ export class DefaultSearch extends React.Component {
         });
     }
 
-	typeChange = (values) => {
-		const { typeChange } = this.props;
-		if (typeChange && typeof typeChange === 'function') {
+    typeChange = (values) => {
+        const {typeChange} = this.props;
+        if (typeChange && typeof typeChange === 'function') {
             typeChange(values);
         }
-	}
-	// onChangeUserName = (e) => {
-	// 	// const { onChange } = this.props;
-	// 	// this.setState({ userName: e.target.value });
-	// 	// this.userName = e.target.value;
-	// 	// onChange && onChange({ userName: e.target.value })
-	// }
-	render() {
+    }
+    // onChangeUserName = (e) => {
+    // 	// const { onChange } = this.props;
+    // 	// this.setState({ userName: e.target.value });
+    // 	// this.userName = e.target.value;
+    // 	// onChange && onChange({ userName: e.target.value })
+    // }
+    render() {
 
-		const { form, showSearchType = false, keywordsOptions = [] } = this.props;
-		const { getFieldDecorator } = form;
-		const { userName } = this.state;
-		const suffix = userName ? <span key='1' style={{ padding: "0 10px" }}><Icon type="close-circle" onClick={this.emitEmpty} /></span> : null;
-		return (
-			<div style={{
-				margin: "0 auto",
-				padding: "0 0 10px 0",
-				// background: "#EDEDED",
-				textAlign: "center"
-			}} className='common-search'>
+        const {form, showSearchType = false, keywordsOptions = []} = this.props;
+        const {getFieldDecorator} = form;
+        const {userName} = this.state;
+        const suffix = userName ? <span key='1' style={{padding: "0 10px"}}><Icon type="close-circle" onClick={this.emitEmpty}/></span> : null;
+        return (
+            <div style={{
+                margin: "0 auto",
+                padding: "0 0 10px 0",
+                // background: "#EDEDED",
+                textAlign: "center"
+            }} className='common-search'>
                 {
                     showSearchType &&
                     <div className='search-type' onClick={this.typeClick}>
@@ -95,9 +96,9 @@ export class DefaultSearch extends React.Component {
                         <Icon type="down" style={{transform: `rotateZ(${this.state.show ? 180 : 0}deg)`}}/>
                         <div id='searchTypePopover' className='search-type-popover' style={{display: this.state.show ? 'block' : 'none'}}>
                             <Checkbox.Group
-								value={keywordsOptions}
-								onChange={this.typeChange}
-								style={{ width: '100%' }}>
+                                value={keywordsOptions}
+                                onChange={this.typeChange}
+                                style={{width: '100%'}}>
                                 <Row>
                                     <Col span={24}>
                                         <Checkbox value='sns_name'>账号名称</Checkbox>
@@ -123,7 +124,7 @@ export class DefaultSearch extends React.Component {
                     })(
                         <Search
                             // onChange={this.onChangeUserName}
-                            style={{ width: "440px" }}
+                            style={{width: "440px"}}
                             placeholder="请输入账号名称、账号ID 、分类标签"
                             suffix={suffix}
                             enterButton
@@ -133,9 +134,9 @@ export class DefaultSearch extends React.Component {
                         />
                     )}
                 </div>
-			</div>
-		);
-	}
+            </div>
+        );
+    }
 }
 
 
