d03176a2f0f27b669bacd5312924659d6191e571
zhangjia <434497825@qq.com>
2020-09-03 10:46:27 +0800

    格式化代码,没有做功能修改

--- src/queryExportTool/actions/index.js
+++ src/queryExportTool/actions/index.js
@@ -1,7 +1,7 @@
 import Interface from '../constants/Interface'
 // import { createHttpAction } from 'redux-action-extend'
 import createHttpAction from '@/store/createHttpAction'
-import { createAction } from 'redux-actions'
+import {createAction} from 'redux-actions'
 import api from "../../api";
 
 export * from './filter'
@@ -25,109 +25,109 @@ export const getAccountList = (params) => (dispatch) => {
         8: Interface.videoSearch // 抖音
     }
 
-	return api.post(interfaceKey[params.groupType], params).then((response) => {
-		const { data, } = response
-		dispatch({
-			type: getAccountList_success,
-			payload: {
-				data,
-				...params,
-			}
-		})
-		return { data }
-	})
+    return api.post(interfaceKey[params.groupType], params).then((response) => {
+        const {data,} = response
+        dispatch({
+            type: getAccountList_success,
+            payload: {
+                data,
+                ...params,
+            }
+        })
+        return {data}
+    })
 }
 export const getAccountList_success = "getAccountList_success"
 
 //批量查号列表
 export const {
-	getBatchSearch,
-	getBatchSearch_success
-} = createHttpAction('getBatchSearch', Interface.getBatchSearch, { method: 'post' })
+    getBatchSearch,
+    getBatchSearch_success
+} = createHttpAction('getBatchSearch', Interface.getBatchSearch, {method: 'post'})
 //清空查询数据
 export const cleanBatchSearch = createAction('cleanBatchSearch', () => {
-	return {};
+    return {};
 })
 //批量查号下载
 export const {
-	batchAccountExport,
-	batchAccountExport_success
+    batchAccountExport,
+    batchAccountExport_success
 } = createHttpAction('batchAccountExport', Interface.batchAccountExport, {
-	method: 'post'
+    method: 'post'
 })
 
 //账号详情
 //账号详情---基本信息
 export const {
-	getBaseInfo,
-	getBaseInfo_success
-} = createHttpAction('getBaseInfo', Interface.getBaseInfo, { isTrack: true })
+    getBaseInfo,
+    getBaseInfo_success
+} = createHttpAction('getBaseInfo', Interface.getBaseInfo, {isTrack: true})
 //账号详情---账号评价
 export const {
-	getDegreeList,
-	getDegreeList_success
+    getDegreeList,
+    getDegreeList_success
 } = createHttpAction('getDegreeList', Interface.getDegreeList)
 //账号详情--账号评价滚动条
 export const {
-	addGetDegreeList,
-	addGetDegreeList_success
+    addGetDegreeList,
+    addGetDegreeList_success
 } = createHttpAction('addGetDegreeList', Interface.getDegreeList)
 
 //账号详情---最近应约价
 export const {
-	getRecentReservationOrderPriceList,
-	getRecentReservationOrderPriceList_success
+    getRecentReservationOrderPriceList,
+    getRecentReservationOrderPriceList_success
 } = createHttpAction('getRecentReservationOrderPriceList', Interface.getRecentReservationOrderPriceList)
 //账号详情---获取最近应约价格数量
-export const{
-  getRecentReservationOrderPriceNum
-} = createHttpAction('getRecentReservationOrderPriceNum',Interface.getRecentReservationOrderPriceNum)
+export const {
+    getRecentReservationOrderPriceNum
+} = createHttpAction('getRecentReservationOrderPriceNum', Interface.getRecentReservationOrderPriceNum)
 //账号详情---最近应约价--滚动条
 export const {
-	AddGetPriceList,
-	AddGetPriceList_success
+    AddGetPriceList,
+    AddGetPriceList_success
 } = createHttpAction('AddGetPriceList', Interface.getRecentReservationOrderPriceList)
 
 //获取公共分类
 export const {
-	getCommonClassify,
-	getCommonClassify_success
+    getCommonClassify,
+    getCommonClassify_success
 } = createHttpAction('getCommonClassify', Interface.getCommonClassify, {
-	method: 'get',
-	isHttp: true,
-	ignoreLoading: true,
-	ignoreToast: true,
+    method: 'get',
+    isHttp: true,
+    ignoreLoading: true,
+    ignoreToast: true,
 })
 //静态添加选中结果
 export const addSelectStatic = createAction('addSelectStatic', (value) => {
-	return value
+    return value
 })
 //静态取消选中结果
 export const removeSelectStatic = createAction('removeSelectStatic', (value) => {
-	return value
+    return value
 })
 
 //静态取消选中结果
 export const removeListSelectStatic = createAction('removeListSelectStatic', (value) => {
-  return value
+    return value
 })
 
 //保存精确查询的值
 export const addSelectExactQuery = createAction('addSelectExactQuery', (value) => {
-	return value
+    return value
 })
 
 export const removeSelectExactQuery = createAction('removeSelectExactQuery', (value) => {
-	return value
+    return value
 })
 export const cleanSelectExactQuery = createAction('cleanSelectExactQuery', (value) => {
-	return value
+    return value
 })
 //编辑点击查看详情统计
 export const addLookDetailOrIndexList = createAction('addLookDetailOrIndexList', (value) => {
-	return value
+    return value
 })
 
 let collectHistoryData = createAction('collectHistoryData', history => history);
-export { collectHistoryData };
+export {collectHistoryData};
 
